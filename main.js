d99b707346d5d7193fd94df40da171379e9c7797
portalplayer <portalplayer@crime.su>
2024-05-01 12:37:22 -0400

    Project: Started

---
+++ main.js
@@ -0,0 +1,7 @@
+// Purpose: Main entry-point
+// Created on: 5/1/24 @ 12:35 PM
+
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+console.log("Hello, World!")
